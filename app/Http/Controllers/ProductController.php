429d0fc8e5e0a6015d0ed21e35621f7ac8b64c11
João Pedro Ferraz <joaoferrazp@gmail.com>
2022-04-14 09:38:44 -0300

    refact:remove blank spaces and add last dependecy injector

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,37 +11,30 @@ use App\Http\Controllers\MovementHistoryController;
 use App\Models\MovementHistory;
 use App\Http\Controllers;
 
-
 class ProductController extends Controller
 {
-   /**
-     * @var MovementHistoryController
-     */
-   private $movementHistory;
+    /**
+      * @var MovementHistoryController
+      */
+    private $movementHistory;
 
-   /**
-     * @var Product
-     */
+    /**
+      * @var Product
+      */
     private $product;
 
 
   
     
-    public function __construct(MovementHistoryController $movementHistory,Product $product)
+    public function __construct(MovementHistoryController $movementHistory, Product $product)
     {
-
-        
         $this->movementHistory = $movementHistory;
         $this->product =$product;
-       
     }
 
     public function store(Request $request)
     {
-
-
-
-        $request = $this->data($request);        
+        $request = $this->data($request);
         $this->product->name =  $request->name;
         $this->product->cod =  $request->cod;
         $this->product->supplier =  $request->supplier;
@@ -52,19 +45,15 @@ class ProductController extends Controller
         $this->product->order =  $request->order;
         $this->product->type =  $request->type;
         $this->product->inventory =  $request->inventory;
-
         $this->product->user = auth()->user()->_id;
-
         $this->product->save();
-
-        return redirect('/dashboard')->with('msg','Produto cadastrado com Sucesso');
+        return redirect('/dashboard')->with('msg', 'Produto cadastrado com Sucesso');
         // return  redirect('/');
     }
 
     public function data($request)
     {
         /*data type validation*/
-
         $request["price"] = floatval($request["price"]);
         $request["inventory"] = intval($request["inventory"]);
 
@@ -84,13 +73,9 @@ class ProductController extends Controller
 
         if ($request->hasFile('image') && $request->file('image')->isValid()) {
             $requestImage = $request->image;
-
             $extension = $requestImage->extension();
-
             $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
-
             $requestImage->move(public_path('img/products'), $imageName);
-
             $request->image = $imageName;
         }
         return $request;
@@ -99,43 +84,31 @@ class ProductController extends Controller
     public function myProducts($user)
     {
         $products = $this->product->where('user', '=', $user)->get();
-
-
         return view('master.products.viewMyProducts', ['products' => $products]);
     }
 
     public function editProducts($id)
     {
         $products = $this->product->where('_id', '=', $id)->get();
-
         return view('master.products.viewProduct', ['products' => $products]);
     }
 
     public function update(Request $request)
     {
-
-        $dataHistory = Product::find($request["id"]);
+        $dataHistory = $this->product->find($request["id"]);
         $requestHandled = $this->data($request);
-
         if ($request->hasFile('image') && $request->file('image')->isValid()) {
             $requestImage = $request->image;
             $extension = $requestImage->extension();
             $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
             $requestImage->move(public_path('img/products'), $imageName);
             $request->image = $imageName;
-                }
-
+        }
         if ($request->image == null) {
             $request->image = $dataHistory->image;
         }
-
         if ($request->price != $dataHistory->price || $request->inventory != $dataHistory->inventory) {
-            $teste= new MovementHistory;
-
-            
-          
             $this->movementHistory->productHistory($dataHistory);
-
             $data = [
                 "cod" => $requestHandled['cod'],
                 "name" => $requestHandled['name'],
@@ -148,7 +121,6 @@ class ProductController extends Controller
                 "image" =>   $request->image
             ];
         } else {
-
             $data = [
                 "cod" => $requestHandled['cod'],
                 "name" => $requestHandled['name'],
@@ -164,21 +136,14 @@ class ProductController extends Controller
 
         $user = auth()->user()->_id;
         $this->product->find($request['id'])->update($data);
-
         return redirect('/meusProdutos/' . $user);
     }
 
     public function changeState(Request $request)
     {
         $requestArray = $request->all();
-
         $data = $this->product->find($request['id']);
-
-
-
-
         if (array_key_exists("order", $requestArray)) {
-
             if ($data["order"] == "Pronta Entrega") {
                 $data = ["order" => "Encomenda"];
 
@@ -188,7 +153,6 @@ class ProductController extends Controller
                 $this->product->find($request['id'])->update($data);
             }
         } else {
-
             if ($data["availability"] == "Disponível") {
                 $data = ["availability" => "Indisponivel"];
                 $this->product->find($request['id'])->update($data);
@@ -197,17 +161,11 @@ class ProductController extends Controller
                 $this->product->find($request['id'])->update($data);
             }
         }
-
-
         return redirect('/meusProdutos/editarProdutos/' . $requestArray["id"]);
     }
 
     public function destroy($id)
     {
-       
-
-
-
         $this->product->where('_id', $id)->delete();
         $user = auth()->user()->_id;
         return redirect('/meusProdutos/' . $user);
@@ -220,8 +178,6 @@ class ProductController extends Controller
     }
     public function viewProduct(Request $request)
     {
-
-        $product = new Product();
         $request = $request->all();
         $products = $this->product->find($request["id"]);
 
@@ -234,17 +190,11 @@ class ProductController extends Controller
 
     public function changedStore($id, $quantity)
     {
-
-
         $newInventory = 0;
-
         $product = $this->product->where('_id', $id)->get();
-
         foreach ($product as $key => $value) {
             $newInventory = $value->inventory - $quantity;
-           }
-
-
+        }
         $inventory = ['inventory' => $newInventory];
         $product =$this->product->where('_id', $id)->update($inventory);
     }
